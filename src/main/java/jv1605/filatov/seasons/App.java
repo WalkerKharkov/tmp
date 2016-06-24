eee8ea9eef19b5eb8870dccb8dc7308579fe1e38
WalkerKharkov <energaleks@gmail.com>
2016-06-24 20:48:06 +0300

    seasons

---
+++ src/main/java/jv1605/filatov/seasons/App.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jv1605.filatov.seasons;
+
+
+import java.util.ResourceBundle;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+/**
+ *
+ * @author Front-End
+ */
+public class App {
+    
+    static ResourceBundle bundle;
+    SimpleDateFormat dateFormat;
+    
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+    static Date getDate(String s){
+        return null;
+    }
+    
+}
